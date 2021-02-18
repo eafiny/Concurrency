d0feca896fdd7521702657e2611fc436438b8d10
Evgenia <zhenja_a@bk.ru>
2021-02-19 00:28:23 +0300

    Initial commit

---
+++ src/ConcurrencyArray.java
@@ -0,0 +1,9 @@
+public class ConcurrencyArray {
+    final int size = 10_000_000;
+    final int h = size / 2;
+    float[] arr = new float[size];
+
+    public static void main(String[] args) {
+
+    }
+}
